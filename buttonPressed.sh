8054deaef88547334425f59c4c6318b0c6ed981c
Dirk Klostermann <klostermann.dirk@gmail.com>
2015-03-28 22:46:07 +0100

    improved setup.sh

--- buttonPressed.sh
+++ buttonPressed.sh
@@ -5,10 +5,17 @@
 bus="001"
 device="004"
 
+if [ "$(id -u)" != "0" ]; then
+	echo "Please execute the script with sudo"
+	exit 1
+fi
+
 usbreset /dev/bus/usb/$bus/$device
 gphoto2 --capture-image-and-download --filename shots/shot_%y%m%d%H%M%S.jpg
 processFeh=$(pidof feh)
 echo "instance: $processFeh"
+export DISPLAY=:0.0 
+export XAUTHORITY=/home/pi/.Xauthority
 /usr/bin/feh --hide-pointer --quiet --recursive --reverse --sort name --full-screen --slideshow-delay 5 'shots' &
 sleep 10 && kill $processFeh &
 
